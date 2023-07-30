e2e6c634c336dfcf9a25ade2ae624c43199509e8
EnmmmmOvO <wangjinghan0218@gmail.com>
2023-07-30 15:58:07 +1000

    rebuild the peoject

--- Resnet_LSTM/DataSet.py
+++ Resnet_LSTM/DataSet.py
@@ -21,7 +21,6 @@ class ImageDataset(Dataset):
                                 transforms.RandomHorizontalFlip(p=0.5),
                                 transforms.RandomVerticalFlip(p=0.5),
                                 transforms.ColorJitter(brightness=0.2, contrast=0.1, saturation=0.1, hue=0.1),
-                                transforms.RandomGrayscale(p=0.025),
                                 transforms.ToTensor(),
                                 transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
                                 ]),
@@ -35,7 +34,8 @@ class ImageDataset(Dataset):
         for i in data_dirs:
             image_files_dir = [os.path.join(i, f) for f in os.listdir(i) if f.endswith('.webp')]
             self.image_files.extend(image_files_dir)
-            json_file = [f for f in os.listdir(i) if f.endswith('.json')][0]
+            json_file = [f for f in os.listdir(i) if f.endswith('.json')]
+            json_file = json_file[0]
             with open(os.path.join(i, json_file)) as f:
                 json_data = json.load(f)
             for key in json_data.keys():
@@ -49,6 +49,7 @@ class ImageDataset(Dataset):
         img_name = os.path.basename(img_path)
         image = Image.open(img_path).convert('RGB')
         image = self.transform(image)
+
         prompt = self.json_data[img_name]
         prompt = self.tokenizer(prompt, return_tensors='pt', padding='max_length', max_length=512, truncation=True)
         input_ids = prompt['input_ids']
@@ -56,9 +57,9 @@ class ImageDataset(Dataset):
         return image, input_ids, attention_mask
 
 
-def get_dataloader(data_location):
+def get_dataloader(data_location, batch_size):
     data_set = sorted([i for i in [os.path.join(data_location, item) for item in os.listdir(data_location)]
                        if os.path.isdir(i)])
     train_number = int(len(data_set) * 0.8)
-    return [DataLoader(ImageDataset(data_set[:train_number], mode=0), batch_size=8, shuffle=True),
-            DataLoader(ImageDataset(data_set[train_number:], mode=1), batch_size=8, shuffle=True)]
+    return [DataLoader(ImageDataset(data_set[0:train_number], mode=0), batch_size=batch_size, shuffle=True),
+            DataLoader(ImageDataset(data_set[train_number:], mode=1), batch_size=batch_size, shuffle=True)]
---
+++ Resnet_LSTM/model.py
@@ -0,0 +1,79 @@
+import torch
+import torch.nn as nn
+from torchvision.models import resnet50
+from tqdm import tqdm
+from transformers import BertTokenizer, BertForMaskedLM
+from DataSet import get_dataloader
+
+EPOCH = 10
+BATCH_SIZE = 4
+DATA_PATH = '../DataSet'
+
+
+class Resnet_LSTM(nn.Module):
+    def __init__(self, resnet_model, bert_model):
+        super().__init__()
+        self.vit = resnet_model
+        self.fc = nn.Linear(1000, 512)
+        self.lstm = nn.LSTM(512, bert_model.config.hidden_size, batch_first=True)
+        self.bert = bert_model
+
+    def forward(self, images, attention_mask):
+        vit_output = self.vit(images)
+        vit_output = self.fc(vit_output)
+        lstm_output, _ = self.lstm(vit_output.unsqueeze(1))
+        lstm_output = lstm_output.expand(-1, 512, -1)
+        bert_output = self.bert(inputs_embeds=lstm_output, attention_mask=attention_mask)
+        return bert_output.logits
+
+
+def train(model, dataloader, optimizer, device, bert_model):
+    model.train()
+    total_loss = 0
+    for images, input_ids, attention_mask in tqdm(dataloader, desc="Training"):
+        images = images.to(device)
+        input_ids = input_ids.to(device)
+        attention_mask = attention_mask.to(device)
+        optimizer.zero_grad()
+        output = model(images, attention_mask)
+        loss = nn.CrossEntropyLoss()(output.view(-1, bert_model.config.vocab_size), input_ids.view(-1))
+        loss.backward()
+        optimizer.step()
+        total_loss += loss.item()
+    return total_loss / len(dataloader)
+
+
+def validate(model, dataloader, device, bert_model):
+    model.eval()
+    total_loss = 0
+    with torch.no_grad():
+        for images, input_ids, attention_mask in tqdm(dataloader, desc="Validating"):
+            images = images.to(device)
+            input_ids = input_ids.to(device)
+            attention_mask = attention_mask.to(device)
+            output = model(images, attention_mask)
+            loss = nn.CrossEntropyLoss()(output.view(-1, bert_model.config.vocab_size), input_ids.view(-1))
+            total_loss += loss.item()
+    return total_loss / len(dataloader)
+
+
+def train_model():
+    device = torch.device('cuda' if torch.cuda.is_available() else 'mps')
+
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    resnet_model = resnet50(pretrained=True)
+    bert_model = BertForMaskedLM.from_pretrained('bert-base-uncased')
+
+    trainDataloader, validDataloader = get_dataloader(DATA_PATH, BATCH_SIZE)
+
+    model = Resnet_LSTM(resnet_model, bert_model).to(device)
+    optimizer = torch.optim.Adam(model.parameters())
+
+    for epoch in range(EPOCH):
+        train_loss = train(model, trainDataloader, optimizer, device, bert_model)
+        valid_loss = validate(model, validDataloader, device, bert_model)
+        print(f'Epoch {epoch + 1}, Train Loss: {train_loss}, Valid Loss: {valid_loss}')
+
+        if epoch % 2 == 0:
+            print('saving checkpoint at epoch {}'.format(epoch + 1))
+            torch.save(model.state_dict(), r"checkModel.pth")
