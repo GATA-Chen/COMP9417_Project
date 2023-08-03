2d6f8b6180c13a09ea90a373c442bb480fb04c4d
EnmmmmOvO <wangjinghan0218@gmail.com>
2023-08-03 22:23:32 +1000

    update resnet part

--- Final/Resnet_LSTM_GPT2/model.py
+++ Final/Resnet_LSTM_GPT2/model.py
@@ -11,6 +11,7 @@ from datasets import load_dataset
 
 warnings.filterwarnings('ignore')
 
+# define parameters
 EPOCH = 16
 BATCH_SIZE = 20
 DATA_PATH = 'poloclub/diffusiondb'
@@ -20,12 +21,14 @@ EMBEDDING_DIM = 1024
 MAX_LENGTH = 200
 
 
+# Defining a custom Dataset class for images and prompt
 class ImageDataset(Dataset):
     def __init__(self, mode=0):
         self.tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
         self.tokenizer.pad_token_id = 0
         self.data_set = TRAIN_DATA_SET if mode == 0 else VALID_DATA_SET
         self.dataset = load_dataset(DATA_PATH, self.data_set)['train']
+        # Define image transformations
         self.transform = [
             transforms.Compose([transforms.RandomRotation(30),
                                 transforms.RandomResizedCrop(224),
@@ -48,17 +51,20 @@ class ImageDataset(Dataset):
         image = self.transform(self.dataset[idx]['image'])
 
         prompt = self.dataset[idx]['prompt']
+        # Recode the prompt using a GPT2 tokenizer while getting the attention_mask
         prompt = self.tokenizer.encode_plus(prompt, add_special_tokens=True, max_length=MAX_LENGTH,
                                             padding='max_length',
                                             return_attention_mask=True, return_tensors='pt')
         return image, prompt['input_ids'], torch.tensor(prompt['attention_mask'])
 
 
+# Function to get the training and validation data loaders
 def get_dataloader():
     return [DataLoader(ImageDataset(0), batch_size=BATCH_SIZE, shuffle=True),
             DataLoader(ImageDataset(1), batch_size=BATCH_SIZE, shuffle=True)]
 
 
+# Defining a custom model class combining a ResNet and LSTM for image captioning
 class Resnet_LSTM(nn.Module):
     def __init__(self, resnet_model, gpt2_model, embedding_dim):
         super().__init__()
@@ -68,23 +74,32 @@ class Resnet_LSTM(nn.Module):
         self.gpt2 = gpt2_model
 
     def forward(self, images, attention_mask):
+        # Pass images through the ResNet model and apply linear transformation
         resnet_output = self.resnet(images)
         resnet_output = self.fc(resnet_output)
         lstm_output, _ = self.lstm(resnet_output.unsqueeze(1))
+
+        # Expand the LSTM output to match the maximum sequence length
         lstm_output = lstm_output.expand(-1, MAX_LENGTH, -1)
+
+        # Pass the LSTM output through the GPT2 model
         gpt2_output = self.gpt2(inputs_embeds=lstm_output, attention_mask=attention_mask)
         return gpt2_output.logits
 
 
 def train(model, dataloader, optimizer, device, gpt2_model, scheduler):
+    # Set model to training mode
     model.train()
     total_loss = 0
     for images, input_ids, attention_mask in tqdm(dataloader, desc="Training"):
+        # Transfer data to GPU if available
         images = images.to(device)
         input_ids = input_ids.to(device)
         attention_mask = attention_mask.to(device)
         optimizer.zero_grad()
+        # Forward pass
         output = model(images, attention_mask)
+        # Count loss and update optimizer and scheduler
         loss = nn.CrossEntropyLoss(ignore_index=0)(output.view(-1, gpt2_model.config.vocab_size), input_ids.view(-1))
         loss.backward()
         optimizer.step()
@@ -94,10 +109,12 @@ def train(model, dataloader, optimizer, device, gpt2_model, scheduler):
 
 
 def validate(model, dataloader, device, gpt2_model):
+    # Set model to evaluation mode
     model.eval()
     total_loss = 0
     with torch.no_grad():
         for images, input_ids, attention_mask in tqdm(dataloader, desc="Validating"):
+            # Transfer data to GPU if available
             images = images.to(device)
             input_ids = input_ids.to(device)
             attention_mask = attention_mask.to(device)
@@ -109,15 +126,21 @@ def validate(model, dataloader, device, gpt2_model):
 
 
 def train_model():
-    device = torch.device('cuda' if torch.cuda.is_available() else 'mps')
+    # Determine the device to use
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+    # Initialize the GPT2 tokenizer and Resnet models
     tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
     gpt2_model = GPT2LMHeadModel.from_pretrained('gpt2')
     resnet_model = models.resnet50(pretrained=True)
 
+    # Get data loaders for training and validation
     train_dataloader, valid_dataloader = get_dataloader()
 
+    # Initialize the custom model and transfer it to the device
     model = Resnet_LSTM(resnet_model, gpt2_model, EMBEDDING_DIM).to(device)
+
+    # Initialize the optimizer and the learning rate scheduler
     optimizer = torch.optim.Adam(model.parameters())
     scheduler = transformers.get_linear_schedule_with_warmup(
         optimizer, num_warmup_steps=8000, num_training_steps=len(train_dataloader) * EPOCH
@@ -128,6 +151,7 @@ def train_model():
         valid_loss = validate(model, valid_dataloader, device, gpt2_model)
         print(f'Epoch {epoch + 1}, Train Loss: {train_loss}, Valid Loss: {valid_loss}')
 
+        # Save the model every 5 epochs
         if (epoch + 1) % 5 == 0:
             print('Saving checkpoint at epoch {}'.format(epoch + 1))
             torch.save(model.state_dict(), "checkModel.pth")
--- Final/Resnet_LSTM_GPT2/predict.py
+++ Final/Resnet_LSTM_GPT2/predict.py
@@ -15,23 +15,39 @@ from tqdm import tqdm
 from model import Resnet_LSTM
 from itertools import groupby
 from difflib import SequenceMatcher
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
+# Setting the device to CUDA if available, else to MPS for Mac
 device = torch.device('cuda' if torch.cuda.is_available() else 'mps')
 
 
+# Function to remove repeated words from a sentence
 def remove_repeated_words(sentence):
     return ' '.join(word for word, _ in groupby(sentence.split()))
 
 
+# Function for calculating the cos similarity between two sentences
+def calculate_similarity(text1, text2):
+    vectorizer = TfidfVectorizer().fit_transform([text1, text2])
+    vectors = vectorizer.toarray()
+    cosine_sim = cosine_similarity(vectors)
+    return cosine_sim[0][1]
+
+
+# Defining a custom Dataset class for images
 class ImageDataset(Dataset):
     def __init__(self, path, preprocess):
         self.images = []
         self.image_names = []
         self.preprocess = preprocess
+        # Loop through all the files in the specified path
         for file in glob.glob(join(path, '*')):
             filename = os.path.basename(file)
+            # Skip file if it is not a PNG image
             if 'png' not in filename:
                 continue
+            # Open and convert the image to RGB
             image = Image.open(file).convert('RGB')
 
             self.images.append(image)
@@ -46,11 +62,16 @@ class ImageDataset(Dataset):
         return len(self.images)
 
 
+# Function to load a model from a checkpoint
 def load_model(model_path):
+    # Initialize the GPT-2 tokenizer and add a padding token
     tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
     tokenizer.pad_token_id = 0
+    # Initialize the ResNet-50 model
     resnet_model = resnet50(pretrained=True)
+    # Initialize the GPT-2 model
     gpt2_model = GPT2LMHeadModel.from_pretrained('gpt2')
+    # Initialize the custom model and load the checkpoint
     model = Resnet_LSTM(resnet_model, gpt2_model, 1024)
     model.load_state_dict(torch.load(model_path, map_location='mps'))
     model.to(device)
@@ -58,17 +79,24 @@ def load_model(model_path):
     return model
 
 
+# Function to generate predictions and save them to a CSV file
 def generate_and_save_predictions(model, dataloader):
     model.eval()
+    # Initialize the GPT-2 tokenizer
     tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-    with open('../DataSet_test/part-000001.json', 'r') as f:
+    # Load the test data
+    with open('/Users/enmmmmovo/Downloads/part-000003/part-000003.json', 'r') as f:
         data = json.load(f)
 
+    # Lists to store the results
     image = list()
     sentences = list()
     original = list()
+    quick_similarity = list()
     similarity = list()
+    cos_similarity = list()
 
+    # Loop through the images in the dataloader
     with torch.no_grad():
         for images, image_names in tqdm(dataloader):
             images = images.to(device)
@@ -76,34 +104,45 @@ def generate_and_save_predictions(model, dataloader):
             output = model(images, attention_mask)
             predicted_ids = output.argmax(dim=-1)
             for i in range(predicted_ids.size(0)):
+                # Decode the predicted tokens
                 tokens = predicted_ids[i].tolist()
                 pre = tokenizer.decode(tokens).encode("ascii", errors="ignore").decode()
                 pre = re.sub(",+", ",", pre)
                 pre = remove_repeated_words(re.sub(" +", " ", pre))
+                # Compare the prediction with the original sentence
                 matcher = SequenceMatcher(None, pre, data[image_names[i]]["p"])
+                # Store the results
                 image.append(image_names[i])
                 sentences.append(pre)
                 original.append(data[image_names[i]]["p"])
-                similarity.append(matcher.quick_ratio() * 100)
+                quick_similarity.append(matcher.quick_ratio() * 100)
+                similarity.append(matcher.ratio() * 100)
+                cos_similarity.append(calculate_similarity(pre, data[image_names[i]]["p"]) * 100)
 
+    # Save the results to a CSV file
     with open('output.csv', 'w', newline='') as f:
         writer = csv.writer(f)
-        writer.writerow(["Image", "Generated Sentence", "Original Sentence", "Similarity Score"])
+        writer.writerow(
+            ["Image", "Generated Sentence", "Original Sentence", "quick_ratio", "ratio", "cosine_similarity"])
         for i in range(len(image)):
-            writer.writerow([image[i], sentences[i], original[i], "{:.2f}%".format(similarity[i])])
+            writer.writerow([image[i], sentences[i], original[i], "{:.2f}".format(quick_similarity[i]),
+                             "{:.2f}".format(similarity[i]), "{:.2f}".format(cos_similarity[i])])
 
 
 def main():
+    # Load the model
     model = load_model('checkModel.pth')
-
-    dataset = ImageDataset('../DataSet_test/',
+    # Initialize the dataset and dataloader
+    dataset = ImageDataset('/Users/enmmmmovo/Downloads/part-000003',
                            transforms.Compose([transforms.CenterCrop(224),
                                                transforms.ToTensor(),
                                                transforms.Normalize([0.485, 0.456, 0.406],
                                                                     [0.229, 0.224, 0.225])]))
     dataloader = DataLoader(dataset, batch_size=1, shuffle=False)
+    # Generate and save the predictions
     generate_and_save_predictions(model, dataloader)
 
 
+# If this script is run as the main script, call the main function
 if __name__ == '__main__':
     main()
