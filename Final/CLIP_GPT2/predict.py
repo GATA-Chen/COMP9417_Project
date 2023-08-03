a4ed413e09eed1993852879464922380d49c33e1
EnmmmmOvO <wangjinghan0218@gmail.com>
2023-08-03 22:46:53 +1000

    add clip comments

--- Final/CLIP_GPT2/predict.py
+++ Final/CLIP_GPT2/predict.py
@@ -18,10 +18,13 @@ import PIL.Image
 import clip
 import csv
 
+
 class ImageDataset(Dataset):
     def __init__(self, path, preprocess):
         self.images = []
         self.image_names = []
+
+        # Load all images from the given path and preprocess them
         for file in glob.glob(join(path, '*')):
             image = io.imread(file)
             image = preprocess(Image.fromarray(image)).squeeze(0).to(device)
@@ -31,6 +34,7 @@ class ImageDataset(Dataset):
             self.image_names.append(filename)
 
     def __getitem__(self, item):
+        # Return the preprocessed image and its filename
         return self.images[item], self.image_names[item]
 
     def __len__(self) -> int:
@@ -59,6 +63,7 @@ def generate2(
     with torch.no_grad():
 
         for entry_idx in range(entry_count):
+            # If an embedding is provided, use it, otherwise generate an embedding from the provided tokens or prompt
             if embed is not None:
                 generated = embed
             else:
@@ -68,10 +73,13 @@ def generate2(
 
                 generated = model.gpt2.transformer.wte(tokens)
 
+            # Generate text of the specified length
             for i in range(entry_length):
-
+                # Get the model outputs
                 outputs = model.gpt2(inputs_embeds=generated)
                 outputs = outputs.logits
+
+                # Apply the temperature
                 outputs = outputs[:, -1, :] / (temperature if temperature > 0 else 1.0)
                 sorted_logits, sorted_indices = torch.sort(outputs, descending=True)
                 cumulative_probs = torch.cumsum(
@@ -85,16 +93,27 @@ def generate2(
 
                 indices_to_remove = sorted_indices[sorted_indices_to_remove]
                 outputs[:, indices_to_remove] = filter_value
+
+                # Get the token with the highest probability
                 next_token = torch.argmax(outputs, -1).unsqueeze(0)
+
+                # Get the embedding for the next token
                 next_token_embed = model.gpt2.transformer.wte(next_token)
+
+                # Add the token to the list of tokens
                 if tokens is None:
                     tokens = next_token
                 else:
                     tokens = torch.cat((tokens, next_token), dim=1)
+
+                # Add the token embedding to the list of embeddings
                 generated = torch.cat((generated, next_token_embed), dim=1)
-                # print(torch.argmax(outputs, -1))
+
+                # If the stop token is encountered, end the generation
                 if stop_token_index == next_token.item():
                     break
+
+            # Convert the tokens to text
             output_list = list(tokens.squeeze().cpu().numpy().flatten())
             output_text = tokenizer.decode(output_list)
             generated_list.append(output_text)
@@ -112,6 +131,7 @@ def generate_beam(
         temperature=1.0,
         stop_token: str = ".",
 ):
+    # This function uses beam search to generate text
     model.eval()
     stop_token_index = tokenizer.encode(stop_token)[0]
     sti = tokenizer.encode("!")[0]
@@ -120,7 +140,9 @@ def generate_beam(
     device = next(model.parameters()).device
     seq_lengths = torch.ones(beam_size, device=device)
     is_stopped = torch.zeros(beam_size, device=device, dtype=torch.bool)
+
     with torch.no_grad():
+        # If an embedding is provided, use it, otherwise generate an embedding from the provided tokens or prompt
         if embed is not None:
             generated = embed
         else:
@@ -133,7 +155,11 @@ def generate_beam(
             logits = outputs.logits
             logits = logits[:, -1, :] / (temperature if temperature > 0 else 1.0)
             logits = logits.softmax(-1).log()
+
+            # This block handles the beam search. It keeps track of the scores and generated tokens, and applies the
+            # beam search algorithm
             if scores is None:
+                # This is the first iteration, we just take the top `beam_size` tokens
                 scores, next_tokens = logits.topk(beam_size, -1)
                 generated = generated.expand(beam_size, *generated.shape[1:])
                 next_tokens, scores = next_tokens.permute(1, 0), scores.squeeze(0)
@@ -143,6 +169,9 @@ def generate_beam(
                     tokens = tokens.expand(beam_size, *tokens.shape[1:])
                     tokens = torch.cat((tokens, next_tokens), dim=1)
             else:
+                # In subsequent iterations, we combine the scores with the scores from previous steps, and keep the
+                # top `beam_size` sequences
+                logits[is_stopped] = -float(np.inf)
                 logits[is_stopped] = -float(np.inf)
                 logits[is_stopped, 0] = 0
                 scores_sum = scores[:, None] + logits
@@ -166,6 +195,7 @@ def generate_beam(
             generated = torch.cat((generated, next_token_embed), dim=1)
             is_stopped = is_stopped + next_tokens.eq(stop_token_index).squeeze()
 
+            # If all sequences have stopped, break the loop
             if is_stopped.all():
                 break
     scores = scores / seq_lengths
@@ -180,10 +210,12 @@ def generate_beam(
 
 
 def string_similar(s1, s2):
+    # This function calculates the quick similarity between two strings
     return difflib.SequenceMatcher(None, s1, s2).quick_ratio()
 
 
 def main(filename):
+    # This function loads a model from a file, and uses it to generate text for a set of images
     tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
     model = ClipCaptionModel(10, 512, True, None).to(device)
     model.load_state_dict(torch.load(filename, map_location=device))
@@ -206,14 +238,6 @@ def main(filename):
             cc = generate2(model, tokenizer, embed=prefix_embed)
             score = string_similar(str(data[name]["p"]), str(cc[0]))
             score = str(round(score, 4) * 100) + "%"
-            # if score > 0.8:
-            #     idx += 1
-            #     print("idx:", idx)
-            #     print(i)
-            #     print(str(data[i]["p"]))
-            #     print(cc[0])
-            #     print(score)
-            #     print()
             list_.append([name, str(data[name]["p"]), str(cc[0]), score])
 
     with open("GT_Prediction_result.csv", 'w') as f:
--- Final/CLIP_GPT2/train_clip.py
+++ Final/CLIP_GPT2/train_clip.py
@@ -17,24 +17,36 @@ from torch.utils.data import Dataset, DataLoader
 
 
 def image_encoder(dataset, mode, f3):
+    # Get the list of available CLIP models
     model = clip.available_models()
     print(model)
+    # Get the user input to select the encoding model
     m = int(input("Enter the corresponding index of the above array to select the coding model: "))
+    # Load the selected CLIP model and the associated preprocessing function
     clip_model, preprocess = clip.load(model[m], device=device, jit=False)
+    # Initialize an empty dictionary to store the image embeddings
     idx2embed = {}
+    # Initialize an empty list to store the prompts (captions)
     prompts = list()
     with tqdm(range(dataset.num_rows['train']), desc='Embedding and split prompts') as pbar:
         for i in pbar:
-            # pbar.set_postfix({'The image with id %d is being encoded' % i})
+            # Read the image and apply the preprocessing function
             image = io.imread(str(dataset.data['train']['image'][0][1]))
             image = preprocess(Image.fromarray(image)).unsqueeze(0).to(device)
+
+            # Generate image embeddings using the CLIP model
             with torch.no_grad():
                 clip_embed = clip_model.encode_image(image).cpu()
+
+            # Store the image embeddings in the dictionary and the prompt (caption) in the list
             idx2embed[i] = clip_embed
             prompts.append(str(dataset.data['train']['prompt'][i]))
             pbar.update()
         pbar.close()
+    # Log the number of image embeddings and prompts
     logger.info('num of image embedding and prompts:{}'.format(len(idx2embed)))
+
+    # Save the prompts and image embeddings to a file
     if mode == "train":
         with open(f3, 'wb') as f:
             pickle.dump([prompts, idx2embed], f)
@@ -46,12 +58,17 @@ def image_encoder(dataset, mode, f3):
 
 class makeDataset(Dataset):
     def __init__(self, len_prefix, normalization, datasets, filename, f2):
+        # Store the prefix length and the normalization flag
         self.normalization = normalization
         self.prefix_length = len_prefix
+
+        # Load the GPT2 tokenizer
         self.tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
         self.embed_list = list()
         # data_path = r"DataSet"
         path = r'/kaggle/working/encoder_for_train_5k.pkl'
+
+        # Check if the data has already been processed and saved in a file，If not, process the data and save to a file
         if os.path.isfile(f2):
             with open(f2, 'rb') as f:
                 self.embed_list, self.text_list, self.max_seq_len = pickle.load(f)
@@ -60,11 +77,15 @@ class makeDataset(Dataset):
             self.text_list = list()
             self.embed_list = list()
             max_seq_len = 0
+
+            # Check if the prompts and image embeddings have already been generated and saved in a file
             if os.path.isfile(filename):
                 with open(filename, 'rb') as f:
                     prompts, idx2embd = pickle.load(f)
             else:
                 prompts, idx2embd = image_encoder(datasets, "train")
+
+            # Loop over the prompts and encode them using the GPT2 tokenizer
             with tqdm(range(len(prompts)), desc='Embedding prompts') as pbar:
                 for i in pbar:
                     text_encoder = torch.tensor(self.tokenizer.encode(prompts[i]), dtype=torch.int64)
@@ -73,23 +94,41 @@ class makeDataset(Dataset):
                     max_seq_len = max(max_seq_len, self.text_list[-1].shape[0])
                     pbar.update()
                 pbar.close()
+
+            # Save the text encodings, image embeddings, and maximum sequence length to a file
             with open(f2, 'wb') as f:
                 pickle.dump([self.embed_list, self.text_list, max_seq_len], f)
+
+        # Calculate the maximum sequence length as the mean plus 10 standard deviations of the lengths of all sequences,
+        # but not more than the maximum length of any sequence
         all_len = torch.tensor([len(self.text_list[i]) for i in range(len(self))]).float()
         self.max_seq_len = min(int(all_len.mean() + all_len.std() * 10), int(all_len.max()))
 
     def pad_tokens(self, item: int):
+        # Get the tokens for the given item
         tokens = self.text_list[item]
+
+        # Calculate the number of padding tokens needed
         padding = self.max_seq_len - tokens.shape[0]
+
+        # If padding is needed, add it to the tokens
         if padding > 0:
             tokens = torch.cat((tokens, torch.zeros(padding, dtype=torch.int64) - 1))
             self.text_list[item] = tokens
         elif padding < 0:
             tokens = tokens[:self.max_seq_len]
             self.text_list[item] = tokens
-        mask = tokens.ge(0)  # mask is zero where we out of sequence
+
+        # Create a mask that is zero where we are out of sequence
+        mask = tokens.ge(0)
+
+        # Mask out-of-sequence tokens
         tokens[~mask] = 0
+
+        # Convert the mask to a float tensor
         mask = mask.float()
+
+        # Add a mask for the prefix
         mask = torch.cat((torch.ones(self.prefix_length), mask), dim=0)  # adding prefix mask
         return tokens, mask
 
@@ -106,62 +145,84 @@ class makeDataset(Dataset):
 
 
 def evaluate(model, dev_loader):
+    # Switch the model to evaluation mode
     model.eval()
     #     logger.info("Running evaluation")
     eval_loss = 0.0
     with torch.no_grad():
         for data in dev_loader:
+            # Move data to device
             embed_list, text_list, mask_list = data
             embed_list = embed_list.to(device).float()
             text_list = text_list.to(device, dtype=torch.int64)
             mask_list = mask_list.to(device)
+
+            # Forward pass
             logits = model(embed_list, text_list, mask_list)
             logits = logits[:, model.prefix_len - 1: -1]
-            #             shift_logits = logits[..., p.prefix_len - 1:-1, :].contiguous().view(-1, logits.size(-1))
+
+            # Flatten the labels
             shift_labels = text_list.flatten()
+
+            # Calculate loss
             loss = F.cross_entropy(logits.reshape(-1, logits.shape[-1]), shift_labels, ignore_index=0)
-            #             shift_logits = logits[..., model.prefix_len - 1:-1, :].contiguous().view(-1, logits.size(-1))
-            #             shift_labels = text_list.view(-1)
-            #             loss = F.cross_entropy(shift_logits, shift_labels)
+
+            # Average the loss over all entries in the batch
             loss = loss.mean()
+
+            # Add the batch loss to the total loss
             eval_loss += loss
 
     return eval_loss
 
 
 def train(model, train_loader, dev_loader, optimizer, scheduler, f3):
+    # Switch the model to training mode
     model.train()
     logger.info("start training")
+
+    # Initialize the list to store loss values
     loss_tt = list()
     for epoch in range(p.epochs):
         logger.info('start {}-th epoch training'.format(epoch + 1))
         for batch_idx, data in enumerate(tqdm(train_loader)):
             model.zero_grad()
+
+            # Compute the step number
             step = epoch * len(train_loader) + batch_idx + 1
+
+            # Move data to device
             embed_list, text_list, mask_list = data
             embed_list = embed_list.to(device).float()
             text_list = text_list.to(device, dtype=torch.int64)
             mask_list = mask_list.to(device)
+
+            # Forward pass
             logits = model(embed_list, text_list, mask_list)
+
+            # Flatten the labels
             logits = logits[:, model.prefix_len - 1: -1]
-            #             # shift_logits = logits[..., p.prefix_len - 1:-1, :].contiguous().view(-1, logits.size(-1))
+
+            # Calculate loss
             shift_labels = text_list.flatten()
             loss = F.cross_entropy(logits.reshape(-1, logits.shape[-1]), shift_labels, ignore_index=0)
-            #             shift_logits = logits[..., model.prefix_len - 1:-1, :].contiguous().view(-1, logits.size(-1))
-            #             shift_labels = text_list.view(-1)
-            #             loss = F.cross_entropy(shift_logits, shift_labels)
+            # Perform backpropagation
             loss.backward()
+            # Update the weights, and adjust the learning rate
             optimizer.step()
             scheduler.step()
             optimizer.zero_grad()
-        #             if step % 10 == 0:
+
+        # Evaluate the model on the validation set
         dev_loss = evaluate(model, dev_loader)
-        #             logger.info('test loss at step {} is {}'.format(step, dev_loss.item()))
+        # Switch the model back to training mode
         model.train()
-        #             logger.info('train loss at step {} is {}'.format(step, loss))
+        # Log the losses
         loss_tt.append((step, loss.item(), dev_loss.item()))
         logger.info('test loss at epoch {} is {}'.format(epoch + 1, dev_loss.item()))
         logger.info('train loss at epoch {} is {}'.format(epoch + 1, loss))
+
+        # Save the model every 2 epochs
         if (epoch + 1) % 2 == 0:
             logger.info('saving checkpoint at epoch {}'.format(epoch + 1))
             torch.save(model.state_dict(), r"/kaggle/working/checkModel" + str(epoch + 1) + " .pth")
@@ -177,11 +238,18 @@ class MLP(nn.Module):
 
     def __init__(self, sizes: Tuple[int, ...], bias=True, act=nn.Tanh):
         super(MLP, self).__init__()
+
+        # Create a list to hold the layers of the MLP
         layers = []
         for i in range(len(sizes) - 1):
+            # Add a linear layer
             layers.append(nn.Linear(sizes[i], sizes[i + 1], bias=bias))
+
+            # Add an activation function (except for the last layer)
             if i < len(sizes) - 2:
                 layers.append(act())
+
+        # Create the MLP as a sequence of layers
         self.model = nn.Sequential(*layers)
 
 
@@ -190,54 +258,83 @@ class ClipCaptionModel(nn.Module):
     def __init__(self, prefix_len, clip_size,
                  finetune_gpt2, constant_len=10):
         super(ClipCaptionModel, self).__init__()
+
+        # Load the pretrained GPT-2 model
         self.gpt2 = GPT2LMHeadModel.from_pretrained('gpt2')
-        #         logger.info('succeed to load pretrain gpt2 model')
+
+        # Define the size of the prefix
         self.prefix_size = self.gpt2.transformer.wte.weight.shape[1]
         self.prefix_len = prefix_len
+
+        # Define the projection layer for CLIP embeddings
         self.clip_project = MLP((clip_size, (self.prefix_size * prefix_len) // 2, self.prefix_size * prefix_len))
+
+        # Define whether to finetune the GPT-2 model
         self.finetune_gpt2 = finetune_gpt2
 
     def forward(self, clip_embeds, caption_ids, mask):
-        # caption_inputs_embeds:[bs, caption_len, prefix_size]
+        # Convert caption IDs to embeddings
         caption_embeds = self.gpt2.transformer.wte(caption_ids)
-        # prefix_embeds:[bs, prefix_len, prefix_size]
+
+        # Project CLIP embeddings to the prefix size
         prefix_embeds = self.clip_project(clip_embeds).view(-1, self.prefix_len, self.prefix_size)
-        # embedding_cat:[bs, prefix_len+caption_len, prefix_size]
+
+        # Concatenate the prefix and caption embeddings
         embedding_cat = torch.cat((prefix_embeds, caption_embeds), dim=1)
+
+        # Pass the concatenated embeddings through the GPT-2 model
         out = self.gpt2(inputs_embeds=embedding_cat, attention_mask=mask)
         # logits:[bs, prefix_len+caption_len, prefix_size]
         logits = out.logits
         return logits
 
     def parameters(self, recurse: bool = True):
+        # If finetuning the GPT-2 model, return all parameters, otherwise return only the projection layer parameters
         if self.finetune_gpt2:
             return super(ClipCaptionModel, self).parameters()
         else:
             return self.clip_project.parameters()
 
     def train(self, mode: bool = True):
+        # Check the type of mode
         if not isinstance(mode, bool):
             raise ValueError("training mode is expected to be boolean")
+
+        # Set the training mode
         self.training = mode
         for module in self.children():
             module.train(mode)
+
+        # If not finetuning the GPT-2 model, set it to evaluation mode
         if not self.finetune_gpt2:
             self.gpt2.eval()
         return self
 
 
 def main(filename, f2, f3):
+    # Clear the GPU memory
     torch.cuda.empty_cache()
+
+    # Instantiate the model and move it to the device
     model = ClipCaptionModel(p.prefix_len, p.img_size, p.tune).to(device)
+
+    # Instantiate the dataset
     dataset = makeDataset(p.prefix_len, p.isNormalize, None, filename, f2)
+
+    # Split the dataset into training and validation sets
     test_size = int(p.test_split * len(dataset))
     train_dataset, dev_dataset = torch.utils.data.random_split(dataset,
                                                                [len(dataset) - test_size,
                                                                 test_size])
+    # Instantiate the data loaders
     train_dataloader = DataLoader(train_dataset, batch_size=p.batch_size, shuffle=True)
     dev_loader = 0
     dev_loader = DataLoader(dev_dataset, batch_size=p.batch_size, shuffle=True)
+
+    # Compute the total number of steps
     t_total = len(train_dataloader) * p.epochs
+
+    # Instantiate the optimizer and the learning rate scheduler
     optimizer = transformers.AdamW(model.parameters(), lr=p.lr)
     scheduler = transformers.get_linear_schedule_with_warmup(
         optimizer, num_warmup_steps=int(t_total * 0.1), num_training_steps=t_total
